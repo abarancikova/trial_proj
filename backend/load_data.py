2a25d4e34a91757abc327a79ce6375bbb231b207
abarancikova <alexandra.barancikova@tufts.edu>
2024-05-14 16:19:38 +0200

    Load the dataset and split it into training and validation

---
+++ backend/load_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_dataset(file_path):
+    dataset = pd.read_csv(file_path)
+    return dataset
+
+#load the dataset
+dataset = load_dataset('backend/dataset/arg_quality_rank_30k.csv')
+
+#split the dataset into features and target variable
+X = dataset.drop(columns=['WA', 'MACE-P', 'stance_WA', 'stance_WA_conf']) 
+y = dataset['WA']
+
+#split the dataset into 80% train and 20% valid
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
